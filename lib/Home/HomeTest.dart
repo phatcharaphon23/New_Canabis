b59b539bf6b46d144f3636d3124d9f24ed49bbc3
phatcharaphon23 <phatcharaphon.h@ku.th>
2022-10-23 13:29:43 +0700

    first commit

---
+++ lib/Home/HomeTest.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_application_3/API/LoginAPI.dart';
+import 'package:flutter_application_3/Class/Early_N_Def.dart';
+import 'package:flutter_application_3/Home/Page1.dart';
+import 'package:flutter_application_3/Home/ProBar.dart';
+import 'package:flutter_application_3/Thenme.dart';
+
+class HomeTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: otpColor,
+      ),
+      // drawer: ProBar(user),
+      body: HomeTest(),
+    );
+  }
+}
